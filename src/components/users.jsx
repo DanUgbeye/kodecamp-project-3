8f83d3fcac185c518ccf8691d5d0ad5bc4a124f7
Daniel Ugbeye <ugbeyellionz@gmail.com>
2022-04-25 13:51:34 +0100

    implemented error handling

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,6 +5,7 @@ const Users = () => {
 
     const [ users, setUsers ] = useState([]);
     const [ loading, setLoading ] = useState(true);
+    const [  errorMessage, setErrorMessage] = useState('');
     
     async function fetchData() {
       try{
@@ -12,15 +13,14 @@ const Users = () => {
         if (res.status === 200) {
           const dat = await res.json();
           const data = await JSON.parse(JSON.stringify(dat));
-          // handle data
           if(data) {
-            // console.log(data)
             setUsers(data);
             setLoading(false);
           }
       }
       } catch (error) {
-        console.log('error  : ' + error)
+        setErrorMessage(error.message);
+        setLoading(false);
       }
     }
 
@@ -28,67 +28,33 @@ const Users = () => {
       fetchData()
     }, []);
 
-    // useEffect(() => {
-    //   console.log(users + "users")
-    // }, [ users ]);
-
-    // let egUsers = [
-    //   {
-    //     id: 1,
-    //     name: 'dan',
-    //     username: 'dee',
-    //     email: 'ug@g.com',
-    //     address: {
-    //       suite: 'suite 88',
-    //       street: 'luke street',
-    //       city: 'calabar'
-    //     },
-    //     phone: 'none',
-    //     website: 'www.bullshit.com',
-    //     company: {
-    //       name: 'craziers'
-    //     }
-    //   },
-
-    //   {
-    //     id: 2,
-    //     name: 'daniel',
-    //     username: 'dee',
-    //     email: 'ug@g.com',
-    //     address: {
-    //       suite: 'suite 88',
-    //       street: 'luke street',
-    //       city: 'calabar'
-    //     },
-    //     phone: 'none',
-    //     website: 'www.bullshit.com',
-    //     company: {
-    //       name: 'craziers'
-    //     }
-    //   }
-    // ]
-
-
   return (
     <div className=' flex flex-col items-center'>
 
       { 
         loading && (
-          <div className={ " w-full pt-[10rem] grid place-items-center text-purple-200 text-3xl font-semibold " }>
+          <div className={ " absolute top-[50%] translate-y-[-50%] w-full grid place-items-center text-purple-200 text-3xl font-semibold " }>
             loading...
           </div>
         )
       }
 
+      { 
+        ( !loading && errorMessage ) && (
+          <div className={ " absolute top-[50%] translate-y-[-50%] w-full max-w-[20rem] mx-8 flex border-solid border-2 border-red-500 rounded-md py-[3rem] place-items-center text-red-500 text-2xl font-semibold " }>
+            <span className=' text-[3rem] w-[30%] grid place-items-center'>!</span>
+            <div className=' w-fit grid place-items-center px-4 '>{errorMessage}</div>
+          </div>
+        )
+      }
+
       {
-        ( !loading ) && (
+        ( !loading && !errorMessage ) && (
           users.map(({ id, name, username, email, phone, website, company, address }) => (
             <Card key={id} name={name} username={username} email={email} phone={phone} website={website} company={company} address={address} />
           ))
         )
       }
-
-      
 
     </div>
   )
