2b703fa6f350990199dfa10745090f21c2921362
Daniel Ugbeye <ugbeyellionz@gmail.com>
2022-04-30 00:23:57 +0100

    added other necessary fields

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Users from "./components/users";
 function App() {
   return (
     <main className=" px-6 pb-12 bg-[#76568A] min-h-[100vh] " >
-      <div className=" max-w-2xl min-w-[20rem] flex flex-col self-center md:mx-auto max-h-fit  overflow-hidden ">
+      <div className=" max-w-4xl min-w-[20rem] flex flex-col self-center mx-auto max-h-fit  overflow-hidden ">
         <h1 className=" text-center text-4xl font-semibold pt-4 mb-12 text-[#DBC1ED]  tracking-wide ">
           USERS
         </h1>
